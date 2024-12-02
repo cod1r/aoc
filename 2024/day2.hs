c1e45c3a39f1a641ca648ef2cf510f7fa4fd1782
cod1r <jasonho353@gmail.com>
2024-12-02 02:57:14 -0600

    day2 part2

--- 2024/day2.hs
+++ 2024/day2.hs
@@ -22,6 +22,12 @@ customFoldFnDec n acc =
   else
     (valid, Just n)
 
+everythingBut :: [Int] -> [[Int]]
+everythingBut lst =
+  let withIndices = zip lst [1..length lst] in
+  let filtered = map (\a -> filter (\b -> snd a /= snd b) withIndices) withIndices in
+  map (\lst -> map fst lst) filtered
+
 isValid :: [Int] -> Bool
 isValid lst =
  fst (foldr customFoldFnInc (True, Nothing) lst) || fst (foldr customFoldFnDec (True, Nothing) lst)
@@ -35,4 +41,8 @@ main = do
   let booled = map isValid numbersParsed
   let ans = length $ filter (\x -> x == True) booled
   putStrLn (show ans)
+  let exploded = map everythingBut numbersParsed
+  let mapped = map (\lst -> any isValid lst) exploded
+  let truthy = filter (\x -> x == True) mapped
+  putStrLn (show $ length truthy)
   hClose handle
