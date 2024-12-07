259cba21fb3042e5306a3cb36041fa830f2c45fb
cod1r <jasonho353@gmail.com>
2024-12-07 03:05:31 -0600

    got day7 part2

--- 2024/day7.hs
+++ 2024/day7.hs
@@ -19,6 +19,12 @@ parseListNum line =
   let (targetStr, numList) = foldr (\c (s, l) -> if isNum c then (c:s, l) else if length s > 0 then ("", readNum s : l) else (s, l)) ("", []) line in
     readNum targetStr : numList
 
+lengthNum n count =
+  if n > 0 then
+    lengthNum (n `div` 10) (count + 1)
+  else
+    count
+
 valid :: Int -> [Int] -> Bool
 valid target numbers =
   if length numbers == 1 then
@@ -26,7 +32,17 @@ valid target numbers =
     result == target
   else
     let (first:second:tail) = numbers in
-    valid target (first + second : tail) || valid target (first * second : tail) 
+    valid target (first + second : tail) || valid target (first * second : tail)
+
+valid2 :: Int -> [Int] -> Bool
+valid2 target numbers =
+  if length numbers == 1 then
+    let (result:_) = numbers in
+    result == target
+  else
+    let (first:second:tail) = numbers in
+    valid2 target (first + second : tail) || valid2 target (first * second : tail) ||
+      valid2 target (first *  10 ^ (lengthNum second 0) + second : tail)
 
 main = do
   handle <- openFile "day7.input" ReadMode
@@ -36,4 +52,7 @@ main = do
   let validOrNot = map (\(h:t) -> valid h t) parsed
   let ans = foldr (\(target:_, valid) acc -> if valid then acc + target else acc) 0 (zip parsed validOrNot)
   putStrLn (show ans)
+  let validOrNot2 = map (\(h:t) -> valid2 h t) parsed
+  let ans2 = foldr (\(target:_, valid) acc -> if valid then acc + target else acc) 0 (zip parsed validOrNot2)
+  putStrLn (show ans2)
   hClose handle
