2168a02bfe5e146eb19bb8cad15d8e3eb9b68138
cod1r <jasonho353@gmail.com>
2024-12-08 05:16:34 -0600

    got day8 part1

---
+++ 2024/day8.hs
@@ -0,0 +1,53 @@
+import Data.Ord
+import Data.Maybe
+import Data.Array
+import Data.List
+import Debug.Trace
+import System.IO
+
+type TwoDArray = Array Int (Array Int Char)
+
+buildArray :: [String] -> TwoDArray
+buildArray lst =
+  let mapped = map (\line -> array (1, length line) (zip [1..length line] line)) lst in
+    array (1, length mapped) (zip [1..length mapped] mapped)
+
+getValueAtPoint :: (Int, Int) -> TwoDArray -> Maybe Char
+getValueAtPoint (x, y) array =
+  let (start, end) = bounds array in
+    if y >= start && y <= end then
+      let row = array ! y in
+      let (colStart, colEnd) = bounds row in
+        if x >= colStart && x <= colEnd then
+          Just (row ! x)
+        else
+          Nothing
+    else
+      Nothing
+
+getAntiPoints (px, py) others =
+  map (\(p2x, p2y) -> (px + (px - p2x), py + (py - p2y))) others
+
+getOrdering a b array =
+  let ac = fromJust (getValueAtPoint a array) in
+    let bc = fromJust (getValueAtPoint b array) in
+    if ac < bc then
+      Data.Ord.LT
+    else if bc < ac then
+      Data.Ord.GT
+    else
+      Data.Ord.EQ
+
+main = do
+  handle <- openFile "day8.input" ReadMode
+  contents <- hGetContents handle
+  let splitLines = lines contents
+  let array = buildArray splitLines
+  let antennas = concat (map (\(rIdx, r) -> map (\(cIdx, _) -> (cIdx, rIdx)) (filter (\(_, c) -> c /= '.') (assocs r))) (assocs array))
+  let sorted = sortBy (\p p2 -> getOrdering p p2 array) antennas
+  let grouped = groupBy (\a b -> getValueAtPoint a array == getValueAtPoint b array) sorted
+  let maybeAntiNodes = concat $ concat (map (\g -> map (\p -> getAntiPoints p (filter (\p2 -> p2 /= p) g)) g) grouped)
+  let inBounds = filter (\p -> isJust $ getValueAtPoint p array) maybeAntiNodes
+  let unique = foldr (\x acc -> if not (x `elem` acc) then x:acc else acc) [] inBounds
+  putStrLn (show $ length unique)
+  hClose handle
