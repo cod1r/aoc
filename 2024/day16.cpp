b2c85083fff7198af57163db270faa3bc1e4406b
cod1r <jasonho353@gmail.com>
2024-12-21 01:24:02 -0600

    finally got day16part2

--- 2024/day16.cpp
+++ 2024/day16.cpp
@@ -11,6 +11,7 @@ struct Node {
   int y;
   int direction;
   int score;
+  std::set<std::pair<int, int>> path;
 };
 
 bool operator==(const Node& a, const Node& b) {
@@ -41,8 +42,8 @@ int main(void) {
   }
   std::set<std::pair<int, int>> walls;
   std::priority_queue<Node, std::vector<Node>, decltype(compare)> pq(compare);
-  Node s{-1, -1, RIGHT, 0};
-  Node e{-1, -1, RIGHT, 0};
+  Node s{-1, -1, RIGHT, 0, std::set<std::pair<int, int>>{}};
+  Node e{-1, -1, RIGHT, 0, std::set<std::pair<int, int>>{}};
   for (int i = 0; i < matrix.size(); ++i) {
     for (int j = 0; j < matrix[i].length(); ++j) {
       if (matrix[i][j] == '#') {
@@ -61,56 +62,69 @@ int main(void) {
   std::cout << s;
   std::cout << e;
   std::cout << "\n";
-  std::set<std::pair<int, int>> visited;
+  std::set<std::tuple<int, int, int>> visited;
   pq.push(s);
+  int minScore = -1;
+  std::set<std::pair<int, int>> uniquePointsForBestPaths;
   while (pq.size() > 0) {
     Node current = pq.top();
     pq.pop();
-    visited.insert({current.x, current.y});
+    visited.insert({current.x, current.y, current.direction});
     std::cout << current;
     if (current.x == e.x && current.y == e.y) {
-      return 0;
+      if (minScore == -1) {
+        minScore = current.score;
+      }
+      if (minScore == current.score) {
+        std::cout << "found\n";
+        uniquePointsForBestPaths.merge(current.path);
+      }
     }
     if (walls.count({ current.x, current.y - 1 }) == 0 &&
-      visited.count({ current.x, current.y - 1 }) == 0) {
-      Node upNode{ current.x, current.y - 1, UP };
+      visited.count({ current.x, current.y - 1, UP }) == 0) {
+      Node upNode{ current.x, current.y - 1, UP, 0, current.path };
       if (current.direction != upNode.direction) {
         upNode.score = current.score + 1001;
       } else {
         upNode.score = current.score + 1;
       }
+      upNode.path.insert({ current.x, current.y - 1 });
       pq.push(upNode);
     }
     if (walls.count({ current.x, current.y + 1 }) == 0 &&
-      visited.count({ current.x, current.y + 1 }) == 0) {
-      Node downNode{ current.x, current.y + 1, DOWN };
+      visited.count({ current.x, current.y + 1, DOWN }) == 0) {
+      Node downNode{ current.x, current.y + 1, DOWN, 0, current.path };
       if (current.direction != downNode.direction) {
         downNode.score = current.score + 1001;
       } else {
         downNode.score = current.score + 1;
       }
+      downNode.path.insert({ current.x, current.y + 1 });
       pq.push(downNode);
     }
     if (walls.count({ current.x - 1, current.y }) == 0 &&
-      visited.count({ current.x - 1, current.y }) == 0) {
-      Node leftNode{ current.x - 1, current.y, LEFT };
+      visited.count({ current.x - 1, current.y, LEFT }) == 0) {
+      Node leftNode{ current.x - 1, current.y, LEFT, 0, current.path };
       if (current.direction != leftNode.direction) {
         leftNode.score = current.score + 1001;
       } else {
         leftNode.score = current.score + 1;
       }
+      leftNode.path.insert({ current.x - 1, current.y });
       pq.push(leftNode);
     }
     if (walls.count({ current.x + 1, current.y }) == 0 &&
-      visited.count({ current.x + 1, current.y }) == 0) {
-      Node rightNode{ current.x + 1, current.y, RIGHT };
+      visited.count({ current.x + 1, current.y, RIGHT }) == 0) {
+      Node rightNode{ current.x + 1, current.y, RIGHT, 0, current.path };
       if (current.direction != rightNode.direction) {
         rightNode.score = current.score + 1001;
       } else {
         rightNode.score = current.score + 1;
       }
+      rightNode.path.insert({ current.x + 1, current.y });
       pq.push(rightNode);
     }
   }
+  std::cout << uniquePointsForBestPaths.size() + 1 << std::endl; // adding one because of E
   return 0;
 }
