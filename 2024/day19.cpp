a1d316815a4733a16c7c76cbe403433a104b42e5
cod1r <jasonho353@gmail.com>
2024-12-23 16:05:25 -0600

    finally got day19 part2. surprised i got both parts because i didn't add the last split string -_-

--- 2024/day19.cpp
+++ 2024/day19.cpp
@@ -20,7 +20,16 @@ std::vector<std::string> split(std::string s, std::string splitOn) {
   strings.push_back(subs);
   start = foundIdx + splitOn.length();
  }
+ strings.push_back(s.substr(start, s.length() - start));
  return strings;
+}
+
+template<typename t>
+std::ostream& operator<<(std::ostream& os, const std::vector<t>& v) {
+  for (const auto& e : v) {
+    os << e << " ";
+  }
+  return os;
 }
 
 bool dfs(const std::vector<std::string>& towels, const std::string_view& pattern, std::map<std::string_view, bool>& memo) {
@@ -43,6 +52,24 @@ bool dfs(const std::vector<std::string>& towels, const std::string_view& pattern
   memo.insert({pattern, false});
   return false;
 }
+uint64_t part2dfs(const std::vector<std::string>& towels, const std::string_view& pattern, std::map<std::string_view, uint64_t>& memo) {
+  if (memo.count(pattern)) return memo[pattern];
+  if (pattern == "") {
+    return 1;
+  }
+  uint64_t ways = 0;
+  for (const std::string& t : towels) {
+    if (pattern.find(t) == 0) {
+      const auto& amt = part2dfs(
+        towels,
+        pattern.substr(t.length()),
+        memo);
+      ways += amt;
+    }
+  }
+  memo[pattern] = ways;
+  return ways;
+}
 
 int main(void) {
   std::ifstream input("day19.input");
@@ -61,6 +88,9 @@ int main(void) {
       patterns.push_back(line);
     }
   }
+
+  std::cout << towels << std::endl;
+  std::vector<std::string_view> solvable;
   int ans = 0;
   for (const auto& p : patterns) {
     std::map<std::string_view, bool> memo;
@@ -68,9 +98,17 @@ int main(void) {
     const auto& found = dfs(towels, p, memo);
     if (found) {
       ++ans;
+      solvable.push_back(p);
     }
   }
 
   std::cout << ans << std::endl;
+  uint64_t ans2 = 0;
+  for (const auto& p : patterns) {
+    std::map<std::string_view, uint64_t> memo;
+    uint64_t ways = part2dfs(towels, p, memo);
+    ans2 += ways;
+  }
+  std::cout << ans2 << std::endl;
   return 0;
 }
