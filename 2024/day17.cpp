2e750d0f10e8d0731ed1fa46187aece0894f1192
cod1r <jasonho353@gmail.com>
2024-12-22 19:43:41 -0600

    finally got day17 part2

--- 2024/day17.cpp
+++ 2024/day17.cpp
@@ -7,13 +7,13 @@
 #include <vector>
 #include <string>
 struct Computer {
-  int64_t a = 0;
-  int64_t b = 0;
-  int64_t c = 0;
+  uint64_t a = 0;
+  uint64_t b = 0;
+  uint64_t c = 0;
   std::vector<int> program;
   size_t instruction_pointer = 0;
   std::vector<int> output_buffer;
-  int64_t getOperandValue(int operand) {
+  uint64_t getOperandValue(int operand) {
     switch (operand) {
       case 0:
       case 1:
@@ -36,10 +36,10 @@ struct Computer {
       default: throw std::runtime_error("UNHANDLED COMBO OPERAND");
     }
   }
-  void process(int opcode, int operand) {
+  void process(uint64_t opcode, uint64_t operand) {
     switch (opcode) {
       case 0: {
-        a = a / (int64_t(1) << getOperandValue(operand));
+        a = a >> getOperandValue(operand);
         instruction_pointer += 2;
       } break;
       case 1: {
@@ -67,11 +67,11 @@ struct Computer {
         instruction_pointer += 2;
       } break;
       case 6: {
-        b = a / (int64_t(1) << getOperandValue(operand));
+        b = a >> getOperandValue(operand);
         instruction_pointer += 2;
       } break;
       case 7: {
-        c = a / (int64_t(1) << getOperandValue(operand));
+        c = a >> getOperandValue(operand);
         instruction_pointer += 2;
       } break;
       default: throw std::runtime_error("UNHANDLED OPCODE");
@@ -89,13 +89,15 @@ std::ostream& operator<<(std::ostream& os, std::vector<int> v) {
   }
   return os << "\n";
 }
+
+void brute_force(uint64_t a, const std::vector<int>& program, int program_index);
 int main(void) {
   std::ifstream input("day17.input");
   if (!input.is_open()) {
     throw std::runtime_error("input file cannot be opened");
   }
   std::string line;
-  std::vector<int> register_values;
+  std::vector<uint64_t> register_values;
   std::vector<int> program;
   while (!input.eof()) {
     std::getline(input, line);
@@ -117,5 +119,55 @@ int main(void) {
   Computer computer{ register_values[0], register_values[1], register_values[2], program };
   computer.run();
   std::cout << computer.output_buffer;
+  for (uint64_t i = 0; i <= (1ull << 16); ++i) {
+    brute_force(i, program, program.size() - 1);
+  }
   return 0;
 }
+
+void brute_force(uint64_t a, const std::vector<int>& program, int program_index) {
+  for (int bits = 0; bits < 8; ++bits) {
+    uint64_t b = (bits & 7) ^ 1;
+    uint64_t c = (a | bits) >> b;
+    uint64_t result = ((b^4)^c) & 7;
+    if (result == program[program_index]) {
+      uint64_t newA = a | bits;
+      if (program_index > 0) {
+        brute_force(newA << 3, program, program_index - 1);
+      } else {
+        Computer c{ newA, 0, 0, program };
+        c.run();
+        if (c.output_buffer == program) {
+          std::cout << newA << std::endl;
+          std::cout << c.output_buffer;
+          std::cout << program;
+          std::cout << std::endl;
+        }
+      }
+    }
+  }
+}
+
+
+/*
+ * 2,4,1,1,7,5,0,3,1,4,4,4,5,5,3,0
+ *
+ * register B = register A & 7
+ * register B = register B ^ 1
+ * register C = A / 2^B
+ * register A = A / 2^3
+ * register B = register B ^ 4
+ * register B = B ^ C
+ * output B & 7
+ * set instruction_pointer to 0 if A not 0. else move instruction_pointer + 2
+ *
+ * B = (A & 7) ^ 1
+ * C = A >> B
+ * B = ((B ^ 4) ^ C) & 7
+ * output B
+ * A = A >> 3
+ *
+ * 010
+ * 011
+ * 101
+ */
